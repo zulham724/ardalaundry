cea8cd524a15b506274b16e729d86a913128487a
Rian Eko <rian.eko1213@gmail.com>
2022-02-02 17:16:31 +0700

    update

--- app/Http/Controllers/API/slave/PostController.php
+++ app/Http/Controllers/API/slave/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API\slave;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\File;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -16,6 +18,17 @@ class PostController extends Controller
     public function index()
     {
         //
+        return Post::with([
+            'images',
+            'videos',
+            'bookmarks',
+            'bookmarked',
+            'author',
+            'files'
+        ])
+            ->withCount('comments', 'likes', 'liked', 'readers', 'hasRead as hasRead')
+            ->orderBy('id', 'desc')
+            ->paginate(3);
     }
 
     /**
@@ -26,7 +39,29 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // return dd($request->all());
+        $post = new Post($request->all());
+        $post->author_id = auth('api')->user()->id;
+        $post->slug = Str::random(8);
+        $post->save();
+        if ($request->hasFile('files')) {
+            foreach ($request->file('files') as $f => $file) {
+                // return dd($request->file('files')[$f]->getClientMimeType());
+                $file = new File();
+                $path = $request->file('files')[$f]->store('files', 'public');
+                $file->src = $path;
+                $file->name =
+                    $request->file('files')[$f]->getClientOriginalName();
+                $file->filetype = $request->file('files')[$f]->getClientMimeType();
+                $post->files()->save($file);
+            }
+        }
+
+        return response()->json($post->load([
+            'images',
+            'videos',
+            'files'
+        ]));
     }
 
     /**
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,66 @@ class Post extends Model
 
     protected $guarded = ["id"];
 
+    public function user(){
+        return $this->belongsTo('App\Models\User', 'author_id');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo('App\Models\User', 'author_id', 'id');
+    }
+
+    public function comments_from_other(){
+        return $this->morphMany('App\Models\Comment', 'commentable')->where('user_id', '!=', auth('api')->user() ? auth('api')->user()->id : -1);
+    }
+
     public function comments(){
         return $this->morphMany('App\Models\Comment', "commentable");
     }
+
+    public function reports(){
+        return $this->morphMany('App\Models\Report', 'reportable');
+    }
+
+    public function bookmarked(){
+        $user=auth('api')->user();
+        return $this->morphOne('App\Models\Bookmark', 'bookmarkable')->where('user_id', $user ? $user->id : 0);
+    }
+
+    public function bookmarks(){
+        return $this->morphMany('App\Models\Bookmark', 'bookmarkable');
+    }
+
+    public function likes(){
+        return $this->morphMany('App\Models\Like', 'likeable');
+    }
+
+    public function liked(){
+        $user=auth('api')->user();
+        return $this->morphOne('App\Models\Like', 'likeable')->where('user_id', $user->id);
+    }
+
+    public function readers(){
+        return $this->morphMany('App\Models\Read', 'readable');
+    }
+
+    public function hasRead(){
+        $user = auth('api')->user();
+
+        return $this->morphOne('App\Models\Read', 'readable')->where('user_id', $user->id);
+    }
+
+    public function files(){
+        return $this->morphMany('App\Models\File', "fileable");
+    }
+
+    public function images(){
+        return $this->morphMany('App\Models\File', "fileable")->whereIn('filetype', ['image/jpeg', 'image/png', 'image/jpg']);
+    }
+
+    public function videos(){
+        return $this->morphMany('App\Models\File', "fileable")->whereIn('filetype', ['video/mp4']);
+    }
+   
+
 }
