0b662e4cbddf5168cd7719e11c7855fd601377b4
Rian Eko <rian.eko1213@gmail.com>
2022-02-15 15:23:20 +0700

    Update CheckSubscribe.php

--- app/Http/Middleware/CheckSubscribe.php
+++ app/Http/Middleware/CheckSubscribe.php
@@ -41,7 +41,7 @@ class CheckSubscribe
         }
 
         if ($pass) {
-            return response()->json(new \DateTime($res->expired_date), new \DateTime());
+            return response()->json(new \DateTime($res->expired_date));
             return $next($request);
         } else {
             return response()->json($res);
