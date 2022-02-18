7d7bc9931933fcd2b10430846ff559eac0252891
Rian Eko <rian.eko1213@gmail.com>
2022-02-18 16:04:17 +0700

    update

--- database/seeders/ServiceUnitTableSeeder.php
+++ database/seeders/ServiceUnitTableSeeder.php
@@ -30,5 +30,82 @@ class ServiceUnitTableSeeder extends Seeder
                 'id' => 2,
             ])->save();
         }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Meter']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 3,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Pairs']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 4,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Meter Persegi']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 5,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Dudukan']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 6,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Potong']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 7,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Jasa']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 8,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Load']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 9,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Coin']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 10,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Galon']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 11,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Set']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 12,
+            ])->save();
+        }
+
+        $service_unit = ServiceUnit::firstOrNew(["name" => 'Cm']);
+        if (!$service_unit->exists) {
+            $service_unit->fill([
+                'id' => 13,
+            ])->save();
+        }
     }
 }
